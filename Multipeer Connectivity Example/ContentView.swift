8a0a5697edd8b0e7a33ab3d0d45bcc0b91e0d68d
oeunji <oieunxe@gmail.com>
2026-05-18 20:48:21 +0900

    Initial Commit

---
+++ Multipeer Connectivity Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multipeer Connectivity Example
+//
+//  Created by 이은지 on 5/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multipeer Connectivity Example/Multipeer_Connectivity_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multipeer_Connectivity_ExampleApp.swift
+//  Multipeer Connectivity Example
+//
+//  Created by 이은지 on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Multipeer_Connectivity_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
