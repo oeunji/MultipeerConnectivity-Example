b82f915c6f99a0bff1a1322c227e150015fe5d48
oeunji <oieunxe@gmail.com>
2026-05-24 13:48:37 +0900

    feat: 채팅 메시지 모델 생성

---
+++ Multipeer Connectivity Example/Model/ChatMessage.swift
@@ -0,0 +1,14 @@
+//
+//  ChatMessage.swift
+//  Multipeer Connectivity Example
+//
+//  Created by 이은지 on 5/24/26.
+//
+
+import Foundation
+
+struct ChatMessage: Identifiable {
+    let id = UUID()
+    let text: String
+    let isMine: Bool
+}
